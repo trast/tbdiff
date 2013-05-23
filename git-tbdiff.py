6f48d457856f7d9e8d565951e66c25cef5952ea6
Thomas Rast <trast@inf.ethz.ch>
2013-05-23 11:00:28 +0200

    Add a copyright notice

--- git-tbdiff.py
+++ git-tbdiff.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# git-tbdiff: show the difference between two versions of a topic branch
+#
+# Copyright (c) 2013, Thomas Rast <trast@inf.ethz.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 import hungarian # https://pypi.python.org/pypi/hungarian
